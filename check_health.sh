c7fbaaf0ca89f755c12366d56f3f11ff22b1c06d
cornzange <84967645+cornzange@users.noreply.github.com>
2021-09-16 16:04:23 +0300

    Update check_health.sh

--- check_health.sh
+++ check_health.sh
@@ -12,15 +12,15 @@ do
     # wait for loadbalancer to register
 
     echo "$(date -u) Waiting for Droplet to register with LB"
-    sleep 2m
+    sleep 3
 
     exit 0
   else
-    if [ $fail_count -eq 11 ]; then
+    if [ $fail_count -eq 300 ]; then
       echo "$(date -u) Server unavailable"
       exit 2
     else
-      echo "$(date -u) Attempt ${fail_count}/10: Server not yet available"
+      echo "$(date -u) Attempt ${fail_count}/300: Server not yet available"
       sleep 3
       fail_count=$[$fail_count +1]
     fi
